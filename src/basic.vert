c9b8bfc5fd41cf58bed838871bc9401a22e5fe4c
wartyz <zwenderk@hotmail.com>
2020-07-02 15:23:25 +0200

    C004 Shader Pt2

--- src/basic.vert
+++ src/basic.vert
@@ -1,5 +1,7 @@
-#version 120
+#version 430
+
+attribute vec3 a_position;
 
 void main() {
-    gl_Position = vec4(vec3(0.0), 1.0);
+    gl_Position = vec4(a_position, 1.0);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
+/*
+https://www.youtube.com/watch?v=6spBXIRsvto&list=PL-88NuvRRCqAPrkxlIH3bFdNiKTYhZbuj&index=1
+https://www.youtube.com/watch?v=LuQpOBg_ebk&list=PL-88NuvRRCqAPrkxlIH3bFdNiKTYhZbuj&index=2
+https://www.youtube.com/watch?v=MId3KcqcLic&list=PL-88NuvRRCqAPrkxlIH3bFdNiKTYhZbuj&index=3
+https://www.youtube.com/watch?v=UtM7cZAlT3E&list=PL-88NuvRRCqAPrkxlIH3bFdNiKTYhZbuj&index=4
+8:05
+*/
+
 extern crate sdl2;
 extern crate gl;
+
+mod gl_utility;
 
 use sdl2::video::GLProfile;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+
+use std::ffi::CString;
+
+use gl_utility::shader::{ShaderManager, Shader};
 
 // LLamada de debugging
 extern "system" fn dbg_callback(
@@ -26,6 +40,7 @@ extern "system" fn dbg_callback(
     }
 }
 
+
 fn main() {
     println!("Hello, world!");
 
@@ -35,7 +50,7 @@ fn main() {
 
     let gl_attr = video_subsystem.gl_attr();
     gl_attr.set_context_profile(GLProfile::Core);
-    gl_attr.set_context_version(4, 0);
+    gl_attr.set_context_version(4, 3);
     gl_attr.set_double_buffer(true);
 
     let window = video_subsystem
@@ -48,7 +63,7 @@ fn main() {
         .build()
         .unwrap();
 
-    let ctx = window.gl_create_context().unwrap();
+    let _ctx = window.gl_create_context().unwrap();
 
     // Para cargar los punteros de las funciones openGL se usa load_with()
     gl::load_with(|name| video_subsystem.gl_get_proc_address(name) as *const _);
@@ -62,6 +77,65 @@ fn main() {
     println!("Pixel format en el contexto de la ventana GL {:?}", window.window_pixel_format());
     println!("OpenGL Profile {:?} - OpenGL version {:?}", gl_attr.context_profile(), gl_attr.context_version());
 
+    let mut shader_manager = ShaderManager::init();
+    //let mut shader_manager2 = ShaderManager::init();
+
+    let basic_shader = shader_manager.register(
+        "basic",
+        include_str!("basic.vert"),
+        include_str!("basic.frag"),
+    );
+
+
+    let vertices: Vec<f32> = vec![
+        //x    y    z
+        -0.5, -0.5, 0.0,
+        -0.5, 0.5, 0.0,
+        0.5, 0.5, 0.0,
+        0.5, 0.5, 0.0,
+        0.5, -0.5, 0.0,
+        -0.5, -0.5, 0.0
+    ];
+    // VBO
+    let mut vbo: gl::types::GLuint = 0;
+    unsafe {
+        gl::GenBuffers(1, &mut vbo);  // Creamos VBO
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo); // Lo "enchufamos" en ARRAY_BUFFER
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            // tamaño de data tipe en bytes
+            (vertices.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr,
+            vertices.as_ptr() as *const gl::types::GLvoid, // puntero a datos
+            gl::STATIC_DRAW,
+        );
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+    }
+
+    let a_position_location = basic_shader.get_attribute_location("a_position");
+    // VAO
+    let mut vao: gl::types::GLuint = 0;
+    unsafe {
+        gl::GenVertexArrays(1, &mut vao);
+
+        gl::BindVertexArray(vao);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::EnableVertexAttribArray(a_position_location); // atributo a_position en shader basic.vert
+        gl::VertexAttribPointer(
+            a_position_location, // Indice del atributo de vertices (a_position)
+            3, // Número de componentes de cada vértice
+            gl::FLOAT, // Tipo de dato
+            gl::FALSE, // Normalizado
+            // stride (byte offset entre atributos)
+            (3 * std::mem::size_of::<f32>()) as gl::types::GLint,
+            std::ptr::null(),
+        );
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        gl::BindVertexArray(0);
+    }
+
+    // Usar programa shader
+    basic_shader.use_shader();
     unsafe {
         // Color de fondo
         gl::ClearColor(0.0, 0.0, 0.0, 1.0);
@@ -99,12 +173,26 @@ fn main() {
                     }
                     _ => ()
                 }
-                _ => {}
+                _ => ()
             }
         }
 
         unsafe {
             gl::Clear(gl::COLOR_BUFFER_BIT);
+            // Dibujar triángulo
+            let colors: Vec<f32> = vec![1.0, 0.5, 0.5, 1.0];
+
+            gl::Uniform4fv(
+                basic_shader.get_uniform_location("u_color"),   // uniform position (u_color)
+                1,
+                colors.as_ptr() as *const gl::types::GLfloat,
+            );
+            gl::BindVertexArray(vao);
+            gl::DrawArrays(
+                gl::TRIANGLES, // modo
+                0, // Indice inicial de los arreglos
+                6, // Número de índices
+            )
         }
         window.gl_swap_window();
     }
